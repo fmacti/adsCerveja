5568b33dd2e1cddc4df5d37c54b50850fa86bfb9
fmacti <fer.aeq@gmail.com>
2018-04-04 12:48:35 -0300

    Inclusao Layout

---
+++ src/main/java/com/aula/cerveja/model/Cerveja.java
@@ -0,0 +1,42 @@
+package com.aula.cerveja.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cerveja {
+	
+	@NotBlank(message = "SKU é obrigatório")
+	private String sku;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
+	
+	@Size(min = 3, max = 50, message = "O tamanho da descrição deve estar entre 3 a 50")
+	private String descricao;
+	
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
